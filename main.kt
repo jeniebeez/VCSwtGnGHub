3b55c48aabfaedd35bd8cbf0136f3a1ad3168d7a
jeniebeez <richardloyj@gmail.com>
2021-06-17 00:32:23 +0800

    Update.main.kt2

--- main.kt
+++ main.kt
@@ -1,8 +1,8 @@
 fun getStringLength(obj: Any): Int? {
- if (obj is String) {
-     return obj.length
- }
- return null
+    if (obj is String) {
+        return obj.length
+    }
+    return null
 }
 fun main() {
     fun printLength(obj: Any) {
